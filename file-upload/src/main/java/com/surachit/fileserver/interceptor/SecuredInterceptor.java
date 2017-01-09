b8a249d1acac767b6dd5474821f5ff085c919e7d
Wanchat Damdoung <wanchat111@hotmail.com>
2017-01-09 20:38:04 +0700

    rename

--- file-upload/src/main/java/com/surachit/fileserver/interceptor/SecuredInterceptor.java
+++ file-upload/src/main/java/com/surachit/fileserver/interceptor/SecuredInterceptor.java
@@ -1,4 +1,4 @@
-package com.surchit.fileserver.interceptor;
+package com.surachit.fileserver.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
